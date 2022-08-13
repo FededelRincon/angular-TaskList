a2dae929164b90698f8ffff770dfc8b43f375ea8
FDR <delrincon@gmail.com>
2022-08-13 20:09:02 -0300

    move pages folder outside from components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,10 @@ import { TaskFormComponent } from './components/task-form/task-form.component';
 import { LoginFormComponent } from './components/auth/login-form/login-form.component';
 import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
 import { NavBarComponent } from './components/nav/nav-bar/nav-bar.component';
-import { HomePageComponent } from './components/pages/home-page/home-page.component';
-import { LoginPageComponent } from './components/pages/login-page/login-page.component';
-import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
-import { NotFoundPageComponent } from './components/pages/not-found-page/not-found-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
